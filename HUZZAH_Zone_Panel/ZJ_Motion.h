3884c4b918f6bf149c4fb2f6401114c00197fde3
Zachary J Welch <zacharyjwelch@gmail.com>
2017-06-05 15:47:52 -0400

    Update ZJ_Motion.h

--- HUZZAH_Zone_Panel/ZJ_Motion.h
+++ HUZZAH_Zone_Panel/ZJ_Motion.h
@@ -15,15 +15,6 @@
 
   + completed  - pending  ! acknowledged bug  * self note
 
-  Core Sketchfile to accomplish the following:
-
-  Output is digital and pulls voltage high when motion is detected.
-  The logic for use in this project is to create a decay timer and
-  reset the timer each time motion is detected. Thus "active" motion
-  can be tracked. Once the decay reaches zero it is clear motion has
-  stopped until new motion is detected. If no motion is detected
-  within a set amount of time, the assumption is nothing is present.
-
   vA0.1.0 - Test/Demo
 
   +Initialize the PMS
